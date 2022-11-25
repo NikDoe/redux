c1fa55c36e8cfaf2ed2ad4663fb9e8be63c87195
NikDoe <nikd0e@yandex.ru>
2022-11-25 15:26:05 +0300

    feature(redux): view the single post
    - Install react router dom;
    - Select post by id;
    - Create SinglePostPage component;
    - Add layout component from router dom;
    - throw post id w/ use params;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import PostsLists from './features/posts/PostsLists';
 import AddPostForm from './features/posts/AddPostForm';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import SinglePostPage from './features/posts/SinglePostPage';
 
 function App() {
 	return (
-		<main className="App">
-			<AddPostForm />
-			<PostsLists />
-		</main>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<PostsLists />} />
+
+				<Route path="post">
+					<Route index element={<AddPostForm />} />
+					<Route path=":postId" element={<SinglePostPage />} />
+				</Route>
+			</Route>
+		</Routes>
 	);
 }
 
