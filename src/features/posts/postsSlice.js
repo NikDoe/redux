c31fa426a0fd65c48edaf7bb09c1998ab89e29e8
NikDoe <nikd0e@yandex.ru>
2022-11-25 19:52:02 +0300

    feature(redux): deleting post
    - Create deletePost async thunk;
    - Add builder case for deletePost;
    - Dispatch deletePost;

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -30,8 +30,18 @@ export const updatePost = createAsyncThunk('posts/updatePost', async initialPost
 		const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
 		return response.data;
 	} catch (err) {
-		//return err.message;
-		return initialPost; // only for testing Redux!
+		return err.message;
+	}
+});
+
+export const deletePost = createAsyncThunk('posts/deletePost', async initialPost => {
+	const { id } = initialPost;
+	try {
+		const response = await axios.delete(`${POSTS_URL}/${id}`);
+		if (response?.status === 200) return initialPost;
+		return `${response?.status}: ${response?.statusText}`;
+	} catch (err) {
+		return err.message;
 	}
 });
 
@@ -104,6 +114,15 @@ const postSlice = createSlice({
 				action.payload.date = new Date().toISOString();
 				const posts = state.posts.filter(post => post.id !== id);
 				state.posts = [...posts, action.payload];
+			})
+			.addCase(deletePost.fulfilled, (state, action) => {
+				if (!action.payload?.id) {
+					console.log('Delete could not complete');
+					console.log(action.payload);
+					return;
+				}
+				const { id } = action.payload;
+				state.posts = state.posts.filter(post => post.id !== id);
 			});
 	},
 });
@@ -116,6 +135,6 @@ export const selectPostById = (state, postId) => {
 	return state.posts.posts.find(post => post.id === postId);
 };
 
-export const { postAdded, reactionAdded } = postSlice.actions;
+export const { reactionAdded } = postSlice.actions;
 
 export default postSlice.reducer;
